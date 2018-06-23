37e1cea83fca70106a673c997de2685d81d5d808
jcsouz10 <jeanrt1@hotmail.com>
2018-06-22 23:57:31 -0300

    .

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,23 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: __dirname + './public',
+        path: __dirname + '/public',
         filename: './app.js'
     },
-    devServer:{
-        port:8080,
-        contentBase: './public'
+    devServer: {
+        port: 8080,
+        contentBase: './public',
     },
-    resolve:{
-        extensios: ['','js','jsx'],
-        alias:{
-            module: __dirname  + '/node_modules'
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
     module: {
@@ -26,14 +26,14 @@ module.exports = {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['2015', 'react'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
-            },
-        },{
+            }
+        }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        },{
-            test: /\.woff|.woff2|.tff|.eot|.svg*.*/,
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }
